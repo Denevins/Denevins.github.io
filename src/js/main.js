b9b2d9d3b08435fd3bb37fbe306abd656f2c5f05
Denevins <85125590+Denevins@users.noreply.github.com>
2021-06-25 17:16:58 +0300

    main.js
    сделал скрипт, который запускает css анимацию только тогда, когда контент появляется в рабочей зоне экрана

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,43 @@
-
 $(window).on('load', function () {
-	$preloader = $('.mk-spinner-wrap'),
-	$loader = $preloader.find('.mk-spinner-doublecircle');
-	$loader.fadeOut();
-	$preloader.delay(350).fadeOut('slow');
+    $preloader = $('.mk-spinner-wrap'),
+        $loader = $preloader.find('.mk-spinner-doublecircle');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
 });
 
+$(function () {
+    var element = $(".skills-item");
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 950) {
+            element.addClass("anim");
+        } else {
+            element.removeClass("anim");
+        }
+    });
+
+});
+
+$(function () {
+    var element = $(".calculator section");
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 2800) {
+            element.addClass("anim");
+        } else {
+            element.removeClass("anim");
+        }
+    });
+});
+
+$(function () {
+    var element = $(".stat-item1,.stat-item2,.stat-item3,.stat-item4");
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 4000) {
+            element.addClass("anim");
+        } else {
+            element.removeClass("anim");
+        }
+    });
+});
 
 
 /* const animItems = document.quertySelectorAll('.anim-items');
@@ -21,4 +53,4 @@ if (animItems.length > 0) {
         function offset(el) {
             const rect = el.window.pageXOffset 
         }
-} */ 
+} */
