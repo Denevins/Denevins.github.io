b228d59e1dffe189bbbc6d5a8e0a5bc358a9241e
Denevins <85125590+Denevins@users.noreply.github.com>
2021-07-18 05:01:02 +0300

    main.js
    сделал программу калькулятора, считающую стоимость и сроки заказа, опираясь на ответы пользователя в промпте

--- src/js/main.js
+++ src/js/main.js
@@ -40,6 +40,59 @@ $(function () {
 });
 
 
+let type = ['0 - Сайт Визитка',' 1 - Интернет-магазин',' 2 - Корпоративный сайт'];
+let layout = ['0 - Адаптивная', ' 1 - Резиновая', ' 2 - Pixel perfect'];
+let design = ['0 - Строгий', ' 1 - Вариант 2', ' 2 - Вариант 3'];
+
+
+let typeal = [];
+let types = ''
+while (!types.length)
+    {
+    types = prompt("Тип сайта:  " +  type);
+    if (types === null) break;
+    }
+if ( !types== null ) arr.push (p);
+
+
+let layoutsal = [];
+let layouts = ''
+while (!layouts.length)
+    {
+    layouts = prompt('Тип верстки:  ' + layout);
+    if (layouts === null) break;
+    }
+if ( !layouts== null ) arr.push (p);
+
+
+let designsal = [];
+let designs = ''
+while (!designs.length)
+    {
+    designs = prompt('Дизайн:  ' + design);
+    if (designs === null) break;
+    }
+if ( !designs== null ) arr.push (p);
+
+
+let typecost = [100, 400, 1000];
+let layoutcost = [100, 50, 70];
+let designcost = [100, 200, 300];
+
+let cost = [];
+cost = typecost[types] + layoutcost[layouts] + designcost[designs];
+
+
+let typetime = [2, 7, 14];
+let layouttime = [2, 1, 2];
+let designtime = [3, 5, 10];
+
+let time =[];
+time = typetime[types] + layouttime[layouts] + designtime[designs];
+
+alert("Цена " + cost + "$  " +  "\nСроки: " + time + " дней");
+
+
 /* const animItems = document.quertySelectorAll('.anim-items');
 
 if (animItems.length > 0) {
